b159456a64f0757de43bf79a9e6ab5189ca49896
PolishchukAlexandr <135067688+PolishchukAlexandr@users.noreply.github.com>
2023-11-10 22:12:44 +0200

    Також поки що закоментував щоб не було конфліктів

---
+++ src/js/modal-window/fetchmodal-window.js
@@ -0,0 +1,13 @@
+// fetch('https://books-backend.p.goit.global/books')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(booksData => {
+//     updateModalContent(booksData);
+//   })
+//   .catch(error => {
+//     console.error('There has been a problem with your fetch operation:', error);
+//   });

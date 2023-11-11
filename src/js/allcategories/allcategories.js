6336c25dbd9df22398a3440b9d6b81f2149912bb
Balinskiy <3879342@gmail.com>
2023-11-11 16:48:19 +0200

    upd-allcategories-v.2

--- src/js/allcategories/allcategories.js
+++ src/js/allcategories/allcategories.js
@@ -1,20 +1,65 @@
-import { getCategories } from "./fetchcategories";
+import { getNameCategories, getBooksCategory } from "./fetchcategories";
 
 const listCategories = document.querySelector('.list-categories');
+const listBooks = document.querySelector(".wrapper-categories");
 
-getCategories().then(data => {
-    if (data.length !== 0) {
+// Отримання всіх категорій для сайдбара
+getNameCategories().then(data => {
+    if (data) {
         createMarkup(data);
-    } else {
-        console.log("Sorry, there are no categories here. Please try again.");
-    }
+    } 
+})
+    .catch(error => {
+    console.log(error.message);
 });
 
+// Відмальовка категорій в сайдбарі
 function createMarkup(arr) {
     const markup = arr.map(({ list_name }) => `
-        <li>${list_name}</li>
+        <li class="categories-item">${list_name}</li>
     `).join("");
     listCategories.insertAdjacentHTML('beforeend', markup);
+
+    // Прослуховувач на всі <li>
+    const listItems = document.querySelectorAll('.categories-item');
+    listItems.forEach(item => {
+        item.addEventListener('click', handleListItemClick);
+    });
+}
+
+// Отримання книг по категоріях та створення Н1
+function handleListItemClick(event) {
+    const categoryName = event.target.textContent;
+
+    listBooks.innerHTML = ''; // Очищення попередньої категорії
+    const h1Element = document.createElement('h1');
+    h1Element.textContent = categoryName;
+    listBooks.appendChild(h1Element);
+
+    getBooksCategory(categoryName).then(data => {
+           if (data) {
+               createMarkupBooks(data);
+    } 
+    })
+    .catch(error => {
+        console.log(error.message);
+    });    
+}
+
+// Відмальовка книг окремої категорії
+function createMarkupBooks(arr) {
+        const markup = arr.map(({ book_image, title, author }) => `
+        <div>
+            <ul class="list-wrapper-categories">
+                <li>
+                    <img src="${book_image}" alt="">
+                    <h2>${title}</h2>
+                    <p>${author}</p>
+                </li>
+            </ul>
+        </div>
+    `).join("");
+    listBooks.insertAdjacentHTML('beforeend', markup);
 }
 
 

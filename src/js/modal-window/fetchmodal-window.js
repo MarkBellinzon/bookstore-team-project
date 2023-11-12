b433aeb4a603a92dc681afcb14232861b9cb2f1f
PolishchukAlexandr <135067688+PolishchukAlexandr@users.noreply.github.com>
2023-11-12 21:37:19 +0200

    закинув поки так,щоб не було потім проблем,бо буду пробувати всі методи підключення

--- src/js/modal-window/fetchmodal-window.js
+++ src/js/modal-window/fetchmodal-window.js
@@ -11,3 +11,23 @@
 //   .catch(error => {
 //     console.error('There has been a problem with your fetch operation:', error);
 //   });
+
+fetch(`https://books-backend.p.goit.global/books/${bookId}`)
+  .then(response => response.json())
+  .then(data => {
+    const book = data;
+    if (book) {
+      renderStats(book);
+      updateButton(
+        bookId,
+        book.title,
+        book.author,
+        book.description,
+        book.book_image,
+        book.publisher
+      );
+    } else {
+      console.error('The book object is empty.');
+    }
+  })
+  .catch(error => console.error(error));
--- src/js/modal-window/modal-window.js
+++ src/js/modal-window/modal-window.js
@@ -1,61 +1,65 @@
-// const bookElements = document.querySelectorAll('.book');
-// const modal = document.getElementById('bookModal');
-// const bookInfo = document.getElementById('bookInfoModal');
+const bookElements = document.querySelectorAll('.book');
+const modal = document.getElementById('bookModal');
+const bookInfo = document.getElementById('bookInfoModal');
 
-// bookElements.forEach(bookElement => {
-//   bookElement.addEventListener('click', () => {
-//     const bookData = getBookData();
-//     updateModalContent(bookData);
-//     modal.style.display = 'block';
-//     document.addEventListener('keydown', handleEscKeyPress);
-//   });
-// });
+bookElements.forEach(bookElement => {
+  bookElement.addEventListener('click', () => {
+    const bookData = getBookData();
+    updateModalContent(bookData);
+    modal.style.display = 'block';
+    document.addEventListener('keydown', handleEscKeyPress);
+  });
+});
 
-// function updateModalContent(bookData) {
-//   const title = bookData.list_name;
-//   const author = bookData.author;
-//   const description = bookData.description;
-//   const image = bookData.book_image;
+function updateModalContent(bookData) {
+  const title = bookData.list_name;
+  const author = bookData.author;
+  const description = bookData.description;
+  const image = bookData.book_image;
+  const amazon = bookData.buyLinks[0];
+  const appleBooks = bookData.buyLinks[1];
+  bookInfoModal.innerHTML = `
+  <img class="modal-img" src="${image}" alt="${title}">
+    <h3 class="modal-title">${title}</h3>
+    <p class="modal-author">Author: ${author}</p>
+    <p class="modal-description">${description}</p>
+    <a href="${amazon}" target="_blank" rel="noopener noreferrer" class="modal-link">Amazon</a>
+    <a href="${appleBooks}" target="_blank" rel="noopener noreferrer" class="modal-link">Apple Books</a>
+  `;
+}
 
-//   bookInfoModal.innerHTML = `
-//   <img class="modal-img" src="${image}" alt="${title}">
-//     <h3 class="modal-title">${title}</h3>
-//     <p class="modal-author">Author: ${author}</p>
-//     <p class="modal-description">${description}</p>
-//   `;
-// }
+function handleEscKeyPress(event) {
+  if (event.key === 'Escape' || event.keyCode === 27) {
+    modal.style.display = 'none';
+    document.removeEventListener('keydown', handleEscKeyPress);
+  }
+}
 
-// function handleEscKeyPress(event) {
-//   if (event.key === 'Escape' || event.keyCode === 27) {
-//     modal.style.display = 'none';
-//     document.removeEventListener('keydown', handleEscKeyPress);
-//   }
-// }
+const closeButton = document.querySelector('.close-modal-btn');
 
-// const closeButton = document.querySelector('.close-modal-btn');
+function closeModal() {
+  modal.style.display = 'none';
+  document.removeEventListener('keydown', handleEscKeyPress);
+}
 
-// function closeModal() {
-//   modal.style.display = 'none';
-//   document.removeEventListener('keydown', handleEscKeyPress);
-// }
+closeButton.addEventListener('click', closeModal);
 
-// closeButton.addEventListener('click', closeModal);
+// Обробник натискання на кнопку додавання в корзину
+const shoppingButton = document.getElementById('shoppingButton');
+const successMessage = document.getElementById('successMessage');
+let inShoppingList = false;
 
-// // Обробник натискання на кнопку додавання в корзину
-// const shoppingButton = document.getElementById('shoppingButton');
-// const successMessage = document.getElementById('successMessage');
-// let inShoppingList = false;
-
-// shoppingButton.addEventListener('click', () => {
-//   if (inShoppingList) {
-//     shoppingButton.textContent = 'Add to shopping list';
-//     successMessage.style.display = 'none';
-//     inShoppingList = false;
-//   } else {
-//     shoppingButton.textContent = 'Remove from the shopping list';
-//     successMessage.textContent =
-//       'Congratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.';
-//     successMessage.style.display = 'block';
-//     inShoppingList = true;
-//   }
-// });
+shoppingButton.addEventListener('click', () => {
+  if (inShoppingList) {
+    shoppingButton.textContent = 'Add to shopping list';
+    successMessage.style.display = 'none';
+    inShoppingList = false;
+  } else {
+    shoppingButton.textContent = 'Remove from the shopping list';
+    successMessage.textContent =
+      'Congratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.';
+    successMessage.style.display = 'block';
+    inShoppingList = true;
+  }
+});
+// тут починається інше

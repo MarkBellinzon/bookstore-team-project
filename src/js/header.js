510c26fdc0b60bd6c47973e91c4322ff888eefb2
@7Karina <karinafrank750@gmail.com>
2023-11-16 01:06:24 +0200

    add files

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,10 @@
+const openBurgerBtn = document.querySelector('.js-open-menu');
+const closeBurgerBtn = document.querySelector('.js-close-btn');
+const mobContainer = document.querySelector('.js-mob-menu');
 
+openBurgerBtn.addEventListener('click', toggleModal);
+closeBurgerBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  mobContainer.classList.toggle('is-hidden');
+}

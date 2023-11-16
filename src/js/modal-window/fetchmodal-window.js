a508a5f7cf0b5e315e373deddce80408304d8519
PolishchukAlexandr <135067688+PolishchukAlexandr@users.noreply.github.com>
2023-11-16 15:24:33 +0200

    (

--- src/js/modal-window/fetchmodal-window.js
+++ src/js/modal-window/fetchmodal-window.js
@@ -1,32 +1,39 @@
-// fetch('https://books-backend.p.goit.global/books')
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     return response.json();
-//   })
-//   .then(booksData => {
-//     updateModalContent(booksData);
-//   })
-//   .catch(error => {
-//     console.error('There has been a problem with your fetch operation:', error);
-//   });
+// function fetchBookData(bookid) {
+//   fetch(`https://books-backend.p.goit.global/books/${bookId}`)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
+//       return response.json();
+//     })
+//     .then(booksData => {
+//       updateModalContent(booksData);
+//     })
+//     .catch(error => {
+//       console.error(
+//         'There has been a problem with your fetch operation:',
+//         error
+//       );
+//     });
+// }
 
-fetch(`https://books-backend.p.goit.global/books/${bookId}`)
-  .then(response => response.json())
-  .then(data => {
-    const book = data;
-    if (book) {
-      renderStats(book);
-      updateButton(
-        bookId,
-        book.list_name,
-        book.author,
-        book.description,
-        book.book_image
-      );
-    } else {
-      console.error('The book object is empty.');
-    }
-  })
-  .catch(error => console.error(error));
+function fetchBookData(bookId) {
+  fetch(`https://books-backend.p.goit.global/books/${bookId}`)
+    .then(response => response.json())
+    .then(data => {
+      const book = data;
+      if (book) {
+        renderStats(book);
+        updateButton(
+          bookId,
+          book.list_name,
+          book.author,
+          book.description,
+          book.book_image
+        );
+      } else {
+        console.error('The book object is empty.');
+      }
+    })
+    .catch(error => console.error(error));
+}

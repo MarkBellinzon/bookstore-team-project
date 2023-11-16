0ec6432d276c55b0b267932f5f6628099beb39d3
VickyUA <vickyy379@gmail.com>
2023-11-16 11:53:55 +0100

    previous version

--- src/js/bestseller/bestseller.js
+++ src/js/bestseller/bestseller.js
@@ -21,21 +21,13 @@ bestsellerDiv.addEventListener('click', seeMoreHandler);
 
 function seeMoreHandler(evt) {
     const seeMore = evt.target;
-    if (!evt.target.classList.contains('js-see-more-btn')) {
-        return;
-    }
     const categoryName = seeMore.parentElement.querySelector('.category-title').textContent;
     bestsellerDiv.innerHTML = "";
     
     fetchCategoryBooks(categoryName)
         .then(books => {
-
-            const arr = categoryName.split(' ');
-            const span = arr.pop();
-            const firstPart = arr.join(' ');
-   
             bestsellerDiv.innerHTML = 
-                `<h1 class='bestseller-title'>${firstPart} <span style='color: #4F2EE8'>${span}</span></h1>
+                `<h1 class='bestseller-title'>${categoryName}</h1>
                 <div class="category-wrapper">
                 <ul class="books-category-list">
                 ${createBookMarkup(books)}                  
